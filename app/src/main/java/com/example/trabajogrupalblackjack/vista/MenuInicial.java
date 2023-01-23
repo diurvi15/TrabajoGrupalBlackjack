34d54abda1737c3166fee2e92c163cbfdebf7d17
diurvi <diegourmente@gmail.com>
2023-01-23 16:40:54 +0100

    Menú con botones

--- app/src/main/java/com/example/trabajogrupalblackjack/vista/MenuInicial.java
+++ app/src/main/java/com/example/trabajogrupalblackjack/vista/MenuInicial.java
@@ -1,8 +1,14 @@
 package com.example.trabajogrupalblackjack.vista;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.trabajogrupalblackjack.R;
 
@@ -12,5 +18,23 @@ public class MenuInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_inicial);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+
+        if(item.getItemId() == R.id.boton1){
+            Intent intent = new Intent(getApplicationContext(), Juego.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }

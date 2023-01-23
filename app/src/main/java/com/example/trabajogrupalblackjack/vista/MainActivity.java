ad1e56a7a6844ea67cf25ba46475852e416284f2
Daniel <dany@MacBook-Pro-de-Daniel.local>
2023-01-23 16:35:36 +0100

    Splash

--- app/src/main/java/com/example/trabajogrupalblackjack/vista/MainActivity.java
+++ app/src/main/java/com/example/trabajogrupalblackjack/vista/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.trabajogrupalblackjack.vista;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 import com.example.trabajogrupalblackjack.R;
 
@@ -12,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler(Looper.myLooper());
+        handler.postDelayed(()-> {
+            Intent intent = new Intent(getApplicationContext(), MenuInicial.class);
+            startActivity(intent);
+            finish();
+        }, 1000);
     }
 }
